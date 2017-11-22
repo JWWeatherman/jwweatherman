02d1a10f30e905e35b37c65f8fb05334deae6071
JWWeatherman <jw@jwweatherman.com>
2017-11-21 22:13:49 -0500

    adds icons dynamically

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,28 @@
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import images from './assets/images'
+  export default {
+    mounted () {
+      /*
+      * adds favicons
+      * */
+      const icons = [
+        `<link href="${images.favicon}" rel="shortcut icon" type="image/x-icon">`,
+        `<link rel="apple-touch-icon" sizes="57x57" href="${images.appleTouchIcons['57']}">`,
+        `<link rel="apple-touch-icon" sizes="114x114" href="${images.appleTouchIcons['114']}">`,
+        `<link rel="apple-touch-icon" sizes="72x72" href="${images.appleTouchIcons['72']}">`,
+        `<link rel="apple-touch-icon" sizes="144x144" href="${images.appleTouchIcons['144']}">`,
+        `<link rel="apple-touch-icon" sizes="60x60" href="${images.appleTouchIcons['60']}">`,
+        `<link rel="apple-touch-icon" sizes="120x120" href="${images.appleTouchIcons['120']}">`,
+        `<link rel="apple-touch-icon" sizes="76x76" href="${images.appleTouchIcons['76']}">`,
+        `<link rel="apple-touch-icon" sizes="152x152" href="${images.appleTouchIcons['152']}">`,
+        `<link rel="apple-touch-icon" sizes="180x180" href="${images.appleTouchIcons['180']}">`
+      ]
+      icons.forEach(i => $('head').prepend($(i)))
+    },
+    name: 'app'
+  }
 </script>
 <style lang="css">
   @import './css/normalize.css';
---
+++ src/assets/images.js
@@ -0,0 +1,14 @@
+export default {
+  favicon: 'http://res.cloudinary.com/doohickey/image/upload/v1511318953/chewie_365_nz7hew_cMk_icon_ipwqlr.ico',
+  appleTouchIcons: {
+    '57': 'http://res.cloudinary.com/doohickey/image/upload/c_scale,w_57/v1510257592/chewie_365_nz7hew.png',
+    '114': 'http://res.cloudinary.com/doohickey/image/upload/c_scale,w_114/v1510257592/chewie_365_nz7hew.png',
+    '72': 'http://res.cloudinary.com/doohickey/image/upload/c_scale,w_72/v1510257592/chewie_365_nz7hew.png',
+    '144': 'http://res.cloudinary.com/doohickey/image/upload/c_scale,w_144/v1510257592/chewie_365_nz7hew.png',
+    '60': 'http://res.cloudinary.com/doohickey/image/upload/c_scale,w_60/v1510257592/chewie_365_nz7hew.png',
+    '120': 'http://res.cloudinary.com/doohickey/image/upload/c_scale,w_120/v1510257592/chewie_365_nz7hew.png',
+    '76': 'http://res.cloudinary.com/doohickey/image/upload/c_scale,w_76/v1510257592/chewie_365_nz7hew.png',
+    '152': 'http://res.cloudinary.com/doohickey/image/upload/c_scale,w_152/v1510257592/chewie_365_nz7hew.png',
+    '180': 'http://res.cloudinary.com/doohickey/image/upload/c_scale,w_180/v1510257592/chewie_365_nz7hew.png'
+  }
+}
