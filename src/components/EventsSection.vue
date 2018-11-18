26ec22a3c358c34ad930e5bde745d7fb773ab89a
Richard Porter <gifted.primate@protonmail.com>
2018-11-18 16:35:54 -0500

    updates course section with new button text and some icons

--- src/components/EventsSection.vue
+++ src/components/EventsSection.vue
@@ -1,18 +1,18 @@
 <template>
 <div id="video-section" class="section">
   <h1 class="heading-2">Cryptocurrency Investing and Trading Course</h1>
-  <h3 class="heading-2">Get the foundation in economics and technology you need to win big.</h3>
-  <button type="button" class="btn btn-success btn-lg" @click="scrollToEle('#contact-section')" >Contact Us to Learn More</button>
+  <div>
+    <img style="height: 200px; width: auto;" src="https://res.cloudinary.com/doohickey/image/upload/v1542575956/noun_Chewbacca_1055391_1_nacdsh.png">
+    <img style="height: 200px; width: auto;" src="https://res.cloudinary.com/doohickey/image/upload/v1542576058/noun_Bitcoin_1510610_kmstfx.png"/>
+  </div>
+  <h3 class="heading-2" style="margin-top: 0;">Get the foundation in economics and technology you need to win big.</h3>
+  <button type="button" class="btn btn-success btn-lg"><a href="/course" target="_blank" style="color: #ffffff;">Sign Up</a></button>
 </div>
 </template>
 
 <script>
-  import animations from '../services/animations'
   export default {
-    name: 'EventsSection',
-    methods: {
-      scrollToEle: animations.scrollToEle
-    }
+    name: 'EventsSection'
   }
 </script>
 
@@ -23,5 +23,10 @@
   align-items: center;
   flex-direction: column;
   padding: 0 0 10% 0;
+}
+
+#video-section .heading-2:first-child {
+  text-decoration: none;
+  padding-bottom: 0;
 }
 </style>
